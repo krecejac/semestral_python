c4c4fb8f4286a6084de0c0f02c1809e55c99dbf1
Jáchym Křeček <krecejac@fit.cvut.cz>
2022-11-09 18:48:30 +0100

    Submission changes in repository

--- homework02/filtering/filtering.py
+++ homework02/filtering/filtering.py
@@ -1,5 +1,25 @@
+"""my howemowork n3"""
 import numpy as np
 
+def check_kernel( kernel: np.array ) -> np.array:
+    """check whether the kernel is in the right format and update it"""
+    row, coll = np.shape(kernel)
+    if row % 2 == 0 or coll % 2 == 0:
+        kernel = np.pad(kernel, [(0, 1), (0, 1)], mode='constant')
+    return kernel
+
+def count_it( image: np.array, kernel: np.array ) -> np.array:
+    """main function for filter"""
+    kernel_shape = np.shape(kernel)
+    image_shape = np.shape(image)
+    new_image = np.zeros(shape=(image_shape), dtype=np.float64)
+    padding = kernel_shape[0]//2
+    ker = kernel_shape[0]
+    image_pad = np.pad(image, pad_width=padding)
+    for row in range(image_shape[0]):
+        for coll in range(image_shape[1]):
+            new_image[row][coll] = np.clip(np.sum( image_pad[row: row + ker, coll: coll+ker] * kernel, dtype=np.float64), 0, 255)
+    return new_image.astype(np.uint8)
 
 def apply_filter(image: np.array, kernel: np.array) -> np.array:
     """ Apply given filter on image """
@@ -8,5 +28,15 @@ def apply_filter(image: np.array, kernel: np.array) -> np.array:
     # A given filter has to be 2 dimensional and square
     assert kernel.ndim == 2
     assert kernel.shape[0] == kernel.shape[1]
-
-    # TODO
+    kernel = check_kernel(kernel)
+    image_shape = image.shape
+    kernel.astype(np.float64)
+    image.astype(np.float64)
+    if image.ndim == 3:
+        new_image = np.empty(image_shape)
+        for i in range(3):
+            new_image[:, :, i:i+1] = count_it( (image[:, :, i:i+1]).reshape( image_shape[0:2] ), kernel ).reshape( image_shape[0:2] + (1,) )
+        image = new_image
+    else:
+        image = count_it( image, kernel )
+    return image.astype(np.uint8)
